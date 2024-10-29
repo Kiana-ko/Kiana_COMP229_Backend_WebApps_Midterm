68d95901cb6d825d3c61259b2997f5030e98eb66
Kiana <kianakooshesh16@gmail.com>
2024-10-28 23:35:14 -0400

    ⚙️ Updated some configs

--- Midterm-Backend/config/config.js
+++ Midterm-Backend/config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "ATLASDB": "mongodb+srv://Kmid:2lSDK2hJXb3knuN7@clusterm.q7rmm.mongodb.net/?retryWrites=true&w=majority&appName=ClusterM"
+    "ATLASDB": "mongodb+srv://Kmid:kianapass@clusterm.q7rmm.mongodb.net/vehicle?retryWrites=true&w=majority&appName=ClusterM"
 }
